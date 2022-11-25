19812d78e18863d023cee42cf1dd1e186ea92e5e
LuYF-Lemon-love <3555028709@qq.com>
2022-11-25 23:26:47 +0800

    提交代码

--- GAMES101/homework1/rasterizer.cpp
+++ GAMES101/homework1/rasterizer.cpp
@@ -16,8 +16,6 @@ rst::ind_buf_id rst::rasterizer::load_indices(const std::vector<Eigen::Vector3i>
     return {id};
 }
 
-// Bresenham's line drawing algorithm
-// Code taken from a stack overflow answer: https://stackoverflow.com/a/16405254
 void rst::rasterizer::draw_line(Eigen::Vector3f begin, Eigen::Vector3f end)
 {
     auto x1 = begin.x();
@@ -27,51 +25,51 @@ void rst::rasterizer::draw_line(Eigen::Vector3f begin, Eigen::Vector3f end)
 
     Eigen::Vector3f line_color = {255, 255, 255};
 
-    int x,y,dx,dy,dx1,dy1,px,py,xe,ye,i;
+    int x, y, dx, dy, dx1, dy1, px, py, xe, ye, i;
 
-    dx=x2-x1;
-    dy=y2-y1;
-    dx1=fabs(dx);
-    dy1=fabs(dy);
-    px=2*dy1-dx1;
-    py=2*dx1-dy1;
+    dx = x2 - x1;
+    dy= y2 - y1;
+    dx1 = fabs(dx);
+    dy1 = fabs(dy);
+    px = 2 * dy1 - dx1;
+    py = 2 * dx1 - dy1;
 
-    if(dy1<=dx1)
+    if(dy1 <= dx1)
     {
-        if(dx>=0)
+        if(dx >= 0)
         {
-            x=x1;
-            y=y1;
-            xe=x2;
+            x = x1;
+            y = y1;
+            xe = x2;
         }
         else
         {
-            x=x2;
-            y=y2;
-            xe=x1;
+            x = x2;
+            y = y2;
+            xe = x1;
         }
+
         Eigen::Vector3f point = Eigen::Vector3f(x, y, 1.0f);
         set_pixel(point,line_color);
-        for(i=0;x<xe;i++)
+        for(i = 0;x < xe; i++)
         {
-            x=x+1;
-            if(px<0)
+            x = x+1;
+            if(px < 0)
             {
-                px=px+2*dy1;
+                px = px + 2 * dy1;
             }
             else
             {
-                if((dx<0 && dy<0) || (dx>0 && dy>0))
+                if((dx < 0 && dy < 0) || (dx > 0 && dy > 0))
                 {
-                    y=y+1;
+                    y = y + 1;
                 }
                 else
                 {
-                    y=y-1;
+                    y = y - 1;
                 }
-                px=px+2*(dy1-dx1);
+                px = px + 2 * (dy1 - dx1);
             }
-//            delay(0);
             Eigen::Vector3f point = Eigen::Vector3f(x, y, 1.0f);
             set_pixel(point,line_color);
         }
@@ -111,7 +109,6 @@ void rst::rasterizer::draw_line(Eigen::Vector3f begin, Eigen::Vector3f end)
                 }
                 py=py+2*(dx1-dy1);
             }
-//            delay(0);
             Eigen::Vector3f point = Eigen::Vector3f(x, y, 1.0f);
             set_pixel(point,line_color);
         }
@@ -159,8 +156,6 @@ void rst::rasterizer::draw(rst::pos_buf_id pos_buffer, rst::ind_buf_id ind_buffe
 
         for (int i = 0; i < 3; ++i)
         {
-            t.setVertex(i, v[i].head<3>());
-            t.setVertex(i, v[i].head<3>());
             t.setVertex(i, v[i].head<3>());
         }
 
